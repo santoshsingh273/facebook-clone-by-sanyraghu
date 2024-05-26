ced9f3a072f8bf46d32ea9a87ead69d67458e26c
Santosh Raghuwanshi <sanyraghu568@gmail.com>
2024-05-26 17:38:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    console.log(`Email: ${email}, Password: ${password}`);
+    
+    // Backend integration can be added here
+});
